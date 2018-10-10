53d7c047a5a3b09dfd2c71c7da82ba7a818edf82
Yoan <yoan.filipe@epsi.fr>
2018-10-10 12:32:35 +0200

    Création d'un fichier pour les notes

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+
 using namespace std;
 
 class etudiant{
@@ -13,6 +15,8 @@ int main(){
 	vector <etudiant> etudiants;
 	string tmp_name, recherche;
 	int tmp_note;
+	ofstream f_notes;
+	f_notes.open("notes.txt");
 	for (int i = 0; i<3;i++){
 		cout << "Entrez le nom de l'étudiant "<<endl;
 		cin >> tmp_name;
@@ -21,6 +25,8 @@ int main(){
 		etudiants.push_back(etudiant());
 		etudiants[i].name = tmp_name;
 		etudiants[i].note =tmp_note;
+		//Ecrire le nom et la note dans le fichier
+		f_notes<<etudiants[i].name<<"                    "<<etudiants[i].note<<endl;
 	}
 	cout << "La liste des étudiants :" << endl;
 	for(int i=0; i<etudiants.size(); i++){
@@ -37,6 +43,20 @@ int main(){
 		i++;
 	}
 	cout << "La note de l'étudiant est : " << etudiants[i].note<<endl;
+
+	/*Correction :
+	cout<<" rechercher un élève : " << endl;
+	cin<<tpm_name;
+	for (int i=0; i<etudiants.size(); i++){
+		cout<< "La note de l'étudiant "<< etudiants[i].name<<" est " <<epsi[i].note<<endl;
+	}
+	*/
+
+	/*Gérer la pertinance des notes
+	ofstream fichier_notes;
+	fichier_notes.open("files.txt");
+	fichier_notes<<"EPSI";
+	fichier_notes.close();*/
 
 	return 0;
 }
